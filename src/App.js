934e1ee1fdc3f71beac74378af7d2e2bdceba3dd
nhat0120230 <nhatnguyen22102003@gmail.com>
2024-04-02 13:50:06 +0700

    welcome function complete

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Welcome from "./component/Welcome";
+
+function App() {
+  return (
+    <Welcome></Welcome>
+  );
+}
+
+export default App;
---
+++ src/component/Welcome.js
@@ -0,0 +1,6 @@
+export default function Welcome(props){
+    console.log(props);
+    return (
+        <h1>Hello {props.name}</h1>
+    );
+}
